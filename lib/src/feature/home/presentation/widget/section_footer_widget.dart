3d90aa6e14aaefc90c65ca93e1fd911abad81d03
Mayank Rai <mayank271993@gmail.com>
2025-01-12 01:21:43 +0100

    web preview release test 1

--- lib/src/feature/home/presentation/widget/section_footer_widget.dart
+++ lib/src/feature/home/presentation/widget/section_footer_widget.dart
@@ -1,5 +1,6 @@
 import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:wahl_analytics/l10n/l10n.dart';
 import 'package:wahl_analytics/src/feature/home/domain/domain.dart';
 import 'package:wahl_analytics/src/feature/home/presentation/bloc/bloc.dart';
@@ -143,10 +144,31 @@ class _CopyrightTextWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DSTextWidget(
-      '©${DateTime.now().year} ${context.localizations.companyName}',
-      color: context.colorPalette.neutral.grey3,
-      style: context.typography.labelSmall,
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        DSTextWidget(
+          '©${DateTime.now().year} ${context.localizations.companyName}',
+          color: context.colorPalette.neutral.grey3,
+          style: context.typography.labelSmall,
+        ),
+        FutureBuilder<PackageInfo>(
+          future: PackageInfo.fromPlatform(),
+          builder: (_, snapshot) {
+            if (snapshot.hasData && snapshot.data != null) {
+              return Padding(
+                padding: EdgeInsets.only(left: context.space(factor: 0.25)),
+                child: DSTextWidget(
+                  '[v${snapshot.data!.version}]',
+                  color: context.colorPalette.neutral.grey3,
+                  style: context.typography.labelSmall,
+                ),
+              );
+            }
+            return const SizedBox.shrink();
+          },
+        ),
+      ],
     );
   }
 }
