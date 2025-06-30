b6ef0d594b395fea5e5588979cdfdbb9a1de47bf
Mayank Rai <mayank271993@gmail.com>
2025-06-30 19:18:16 +0200

    [Feature] Android Release

--- tool/storybook/lib/assets/assets.gen.dart
+++ tool/storybook/lib/assets/assets.gen.dart
@@ -26,23 +26,17 @@ class $AssetsImageGen {
 }
 
 class Assets {
-  Assets._();
+  const Assets._();
 
   static const $AssetsImageGen image = $AssetsImageGen();
 }
 
 class SvgGenImage {
-  const SvgGenImage(
-    this._assetName, {
-    this.size,
-    this.flavors = const {},
-  }) : _isVecFormat = false;
+  const SvgGenImage(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = false;
 
-  const SvgGenImage.vec(
-    this._assetName, {
-    this.size,
-    this.flavors = const {},
-  }) : _isVecFormat = true;
+  const SvgGenImage.vec(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = true;
 
   final String _assetName;
   final Size? size;
@@ -96,7 +90,8 @@ class SvgGenImage {
       placeholderBuilder: placeholderBuilder,
       semanticsLabel: semanticsLabel,
       excludeFromSemantics: excludeFromSemantics,
-      colorFilter: colorFilter ??
+      colorFilter:
+          colorFilter ??
           (color == null ? null : ColorFilter.mode(color, colorBlendMode)),
       clipBehavior: clipBehavior,
       cacheColorFilter: cacheColorFilter,
